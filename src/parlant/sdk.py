9ee154cdf582eed2e7488afd90576f9947bcb291
Dor Zohar <dor@emcie.co>
2026-02-09 12:56:46 +0200

    Add for sdk collections_prefix when creating stores
    Signed-off-by: Dor Zohar <dor@emcie.co>

--- src/parlant/sdk.py
+++ src/parlant/sdk.py
@@ -4994,9 +4994,11 @@ class Server:
                 )
 
             mongo_client: object | None = None
+            mongo_db: DocumentDatabase | None = None
 
-            async def make_mongo_db(url: str, name: str) -> DocumentDatabase:
+            async def make_mongo_db(url: str) -> DocumentDatabase:
                 nonlocal mongo_client
+                nonlocal mongo_db
 
                 if importlib.util.find_spec("pymongo") is None:
                     raise SDKError(
@@ -5012,15 +5014,18 @@ class Server:
                         AsyncMongoClient[Any](url)
                     )
 
-                db = await self._exit_stack.enter_async_context(
-                    MongoDocumentDatabase(
-                        mongo_client=cast(AsyncMongoClient[Any], mongo_client),
-                        database_name=f"parlant_{name}",
-                        logger=c()[Logger],
+                if mongo_db is None:
+                    mongo_db = await self._exit_stack.enter_async_context(
+                        MongoDocumentDatabase(
+                            mongo_client=cast(AsyncMongoClient[Any], mongo_client),
+                            database_name="parlant",
+                            logger=c()[Logger],
+                        )
                     )
-                )
 
-                return db
+                assert mongo_db
+
+                return mongo_db
 
             async def make_persistable_store(t: type[T], spec: str, name: str, **kwargs: Any) -> T:
                 store: T
@@ -5046,8 +5051,9 @@ class Server:
                 elif spec.startswith("mongodb://") or spec.startswith("mongodb+srv://"):
                     store = await self._exit_stack.enter_async_context(
                         t(
-                            database=await make_mongo_db(spec, name),
+                            database=await make_mongo_db(spec),
                             allow_migration=self._migrate,
+                            collections_prefix=name,
                             **kwargs,
                         )  # type: ignore
                     )
@@ -5099,6 +5105,7 @@ class Server:
                     tracer=c()[Tracer],
                     nlp_services_provider=lambda: {"__nlp__": c()[NLPService]},
                     allow_migration=False,
+                    collections_prefix="services",
                 )
             )
 
