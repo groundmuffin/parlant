19faaf4d550e6a8b201982b3dc54cad072a67205
Dor Zohar <dor@emcie.co>
2026-03-15 14:52:24 +0200

    Remove EmcieAuthorizationPolicy
    Signed-off-by: Dor Zohar <dor@emcie.co>

--- src/parlant/adapters/modules/emcie.py
+++ src/parlant/adapters/modules/emcie.py
@@ -1,12 +1,8 @@
 from contextlib import AsyncExitStack
 import os
-from typing import Mapping
-from typing_extensions import override
 
 from httpx import AsyncClient
 from lagom import Container
-from fastapi import FastAPI, Request, WebSocket
-from fastapi.middleware.cors import CORSMiddleware
 
 from parlant.core.tracer import Tracer, CompositeTracer
 from parlant.core.loggers import Logger, CompositeLogger
@@ -14,54 +10,9 @@ from parlant.core.meter import Meter
 from parlant.adapters.tracing.emcie import EmcieTracer
 from parlant.adapters.loggers.emcie import EmcieLogger
 from parlant.adapters.meters.emcie import EmcieMeter
-from parlant.api.authorization import AuthorizationPolicy, Operation, ProductionAuthorizationPolicy
 
 
 EXIT_STACK = AsyncExitStack()
-
-
-class EmcieAuthorizationPolicy(AuthorizationPolicy):
-    def __init__(self, trusted_origins: list[str]) -> None:
-        self._trusted_origins = [origin.lower().rstrip("/") for origin in trusted_origins]
-        self._production_policy = ProductionAuthorizationPolicy()
-
-    async def configure_app(self, app: FastAPI) -> FastAPI:
-        app.add_middleware(
-            CORSMiddleware,
-            allow_origins=["*"],
-            allow_credentials=True,
-            allow_methods=["*"],
-            allow_headers=["*"],
-        )
-
-        return app
-
-    def _is_trusted(self, headers: Mapping[str, str]) -> bool:
-        origin = headers.get("origin", "").lower().rstrip("/")
-        return origin in self._trusted_origins
-
-    @property
-    @override
-    def name(self) -> str:
-        return "emcie"
-
-    @override
-    async def check_permission(self, request: Request, operation: Operation) -> bool:
-        if self._is_trusted(request.headers):
-            return True
-        return await self._production_policy.check_permission(request, operation)
-
-    @override
-    async def check_rate_limit(self, request: Request, operation: Operation) -> bool:
-        if self._is_trusted(request.headers):
-            return True
-        return await self._production_policy.check_rate_limit(request, operation)
-
-    @override
-    async def check_websocket_permission(self, websocket: WebSocket, operation: Operation) -> bool:
-        if self._is_trusted(websocket.headers):
-            return True
-        return await self._production_policy.check_websocket_permission(websocket, operation)
 
 
 async def configure_module(container: Container) -> Container:
@@ -117,7 +68,5 @@ async def configure_module(container: Container) -> Container:
         # Meter not in container, add EmcieMeter
         emcie_meter = await EXIT_STACK.enter_async_context(EmcieMeter())
         container[Meter] = emcie_meter
-
-    container[AuthorizationPolicy] = EmcieAuthorizationPolicy(trusted_origins=[api_url])
 
     return container
